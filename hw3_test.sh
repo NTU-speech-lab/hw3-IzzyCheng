bc6190c3c59756332a015ab65faadd39b03a42fa
Izzy Cheng <qaz30239@gmail.com>
2020-05-16 00:45:56 +0800

    update hw3_test.sh

--- hw3_test.sh
+++ hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/NTU-speech-lab/hw3-IzzyCheng/releases/download/1/CNN_22344.pth
+wget https://github.com/NTU-speech-lab/hw3-IzzyCheng/releases/download/1/CNN_22244.pth
 python CNN_test.py $1 $2
